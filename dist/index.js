c48bbd99e31030f3614bab606db846c629ca34c2
Raistlin916 <raistlin916@gmail.com>
2017-04-26 15:44:48 +0800

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SwpPage = exports.Swp = undefined;
+
+var _Swp = require('./Swp.vue');
+
+var _Swp2 = _interopRequireDefault(_Swp);
+
+var _SwpPage = require('./SwpPage.vue');
+
+var _SwpPage2 = _interopRequireDefault(_SwpPage);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Swp = _Swp2.default;
+exports.SwpPage = _SwpPage2.default;
---
+++ index.js
@@ -0,0 +1,6 @@
+var compiled = require('./compiled/index');
+
+module.exports = {
+    Swp: compiled.Swp,
+    SwpPage: compiled.SwpPage
+};
